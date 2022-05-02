1036431a006f14242f184ea09091b9ba3ce5986d
nadezdatsygankova <nadiapritchina@gamil.com>
2022-05-02 17:10:36 -0400

    fix: change countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(backyard) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let count = 0;
+  for (let i = 0; i < backyard.length; i++) {
+    console.log(backyard[i]);
+    for (let j = 0; j < backyard[i].length; j++) {
+      if (backyard[i][j] === "^^") {
+         count +=1;
+      }
+    }
+  }
+ return count;
+
 };
