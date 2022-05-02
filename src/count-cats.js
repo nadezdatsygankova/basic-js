822eaff581426ea025a401367ac0302e0ccd62bd
Nadia Tsygankova <47821694+nadezdatsygankova@users.noreply.github.com>
2022-05-02 17:18:25 -0400

    Update count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,17 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(backyard) {
-  // throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-  let count = 0;
-  for (let i = 0; i < backyard.length; i++) {
-    console.log(backyard[i]);
-    for (let j = 0; j < backyard[i].length; j++) {
-      if (backyard[i][j] === "^^") {
-         count +=1;
+  let number = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    let k = matrix[i];
+    for (let j = 0; j < k.length; j++) {
+      if (k[j] === '^^') {
+        number++;
       }
     }
   }
- return count;
+  // if (matrix === undefined) {
+  //   return false;
+  // }
+  return number;
 
 };
